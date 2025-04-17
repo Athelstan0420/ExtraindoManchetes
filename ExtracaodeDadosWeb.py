0bb0c91bc69f3f3d73d09de6a64add0c4bd23241
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-04-17 00:43:19 -0300

    Update ExtracaodeDadosWeb.py

--- ExtracaodeDadosWeb.py
+++ ExtracaodeDadosWeb.py
@@ -7,49 +7,29 @@ from bs4 import BeautifulSoup
 import requests
 import openpyxl 
 
-# try:
-#     def acessarplanilha():
-#         workbook = openpyxl.load_workbook('/home/adriel/Documentos/Develop/ExtracaoMachetes/Sites_Classes.xlsx') #Para carregar a planilha;
-#         pagina_clientes = workbook['Página1'] 
-#         for linha in pagina_clientes.iter_rows(min_row=2):
-#             sites = linha[0].value 
-#             nomeClasses = linha[1].value
-#             dominio = sites
-#             classes = nomeClasses
-#             print(dominio, classes)
-# except:
-#     print("Erro!")
-
-# acessarplanilha()
-
 def Extrair_informalções_site():
     try:
-        # workbook = openpyxl.load_workbook('/home/adriel/Documentos/Develop/ExtracaoMachetes/Sites_Classes.xlsx') #Para carregar a planilha;
-        # pagina_sites = workbook['Página1'] 
-        # for linha in pagina_sites.iter_rows(min_row=2):
-        #     sites = linha[0].value 
-        #     nomeClasses = linha[1].value
-        #     dominio = sites
-        #     classes = nomeClasses
-        #     #print(dominio, classes)
+        workbook = openpyxl.load_workbook('/home/adriel/Documentos/Develop/ExtracaoMachetes/Sites_Classes.xlsx') #Para carregar a planilha;
+        pagina_sites = workbook['Página1'] 
+        for linha in pagina_sites.iter_rows(min_row=2):
+            sites = linha[0].value 
+            nomeClasses = linha[1].value
             #Requisicao:
-            url = "https://www.cnnbrasil.com.br/" #Site que você quer extrair as informações.
+            url = sites #Site que você quer extrair as informações.
             headers = {"User-Agent":"Mozilla/5.0"}#Simula um usuário acessando o navegador e não um programa.
             requisicao = requests.get(url, headers=headers)#Requisicao em si.
-            code_pagina = BeautifulSoup(requisicao.text, "html.parser") #Pega o código fonte em html.
-            titulos_paginas = code_pagina.find_all(class_="single-header__title")#Solicita a classe de títulos das materias
-            for i in titulos_paginas:
-                print(i.text) #printar todos títulos já extraídos em forma de texto.
-
-
-            # #Validação:
-            # if requisicao.status_code == 200: #Se o status da requisição dor igual a 200.
-               
+            code_pagina = BeautifulSoup(requisicao.text, "html.parser") #Pega o código fonte em html.  
+            titulos_paginas = code_pagina.find_all(class_=f"{nomeClasses}")#Solicita a classe de títulos das materias
+             # #Validação:
+            if requisicao.status_code == 200: #Se o status da requisição dor igual a 200.
+                for i in titulos_paginas:
+                    print(i.text) #printar todos títulos já extraídos em forma de texto.
+                    
         #200 = Requisição que deu certo.
         #301 = Redirecionado para outro site.
         #404 = Página não existe.      
-        #    
     except:
         print("Erro!")
 
 Extrair_informalções_site()
+
